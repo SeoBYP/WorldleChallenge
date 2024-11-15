1248b04fc150c82af711f4af5d27b6098ffe38f7
SeoBYP <byeongpilseo@gmail.com>
2024-11-15 11:27:45 +0900

    Create Project

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Happy developing ✨')
+// const timeH1 = document.querySelector(".time");
+// const startTime = new Date();
+//
+// function SetTime()
+// {
+//     const currentTime = new Date();
+//     const timer = new Date(currentTime - startTime);
+//     const hours = timer.getHours();
+//     const minutes = timer.getMinutes().toString();
+//     const seconds = timer.getSeconds().toString();
+//     timeH1.innerHTML = `${minutes.padStart(2,"0")}:${seconds.padStart(2,"0")}`;
+// }
+//
+// setInterval(SetTime,1000);
